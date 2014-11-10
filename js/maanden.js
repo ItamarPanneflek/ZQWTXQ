04b4ccd811582d1b87c1d1dcd6f37a48ebc69616
ItamarPanneflek <itamar.panneflek@gmail.com>
2014-11-10 15:46:07 +0100

    User bestanden toevoegen
    Json uit users toevoegen

---
+++ js/maanden.js
@@ -0,0 +1,52 @@
+{
+    "maanden": [
+        {
+            "naam": "januari",
+            "cijfer": "1"
+        },
+        {
+            "naam": "februari",
+            "cijfer": "2"
+        },
+        {
+            "naam": "maart",
+            "cijfer": "3"
+        },
+        {
+            "naam": "april",
+            "cijfer": "4"
+        },
+        {
+            "naam": "mei",
+            "cijfer": "5"
+        },
+        {
+            "naam": "juni",
+            "cijfer": "6"
+        },
+        {
+            "naam": "juli",
+            "cijfer": "7"
+        },
+        {
+            "naam": "augustus",
+            "cijfer": "8"
+        },
+        {
+            "naam": "september",
+            "cijfer": "9"
+        },
+        {
+            "naam": "oktober",
+            "cijfer": "10"
+        },
+        {
+            "naam": "november",
+            "cijfer": "11"
+        },
+        {
+            "naam": "december",
+            "cijfer": "12"
+        }
+    ]
+}
